d8c0d89a3a633f5ba3a0df3855f5386a275d1a8b
Abramo-D <dorio.abr@gmail.com>
2026-01-15 16:58:54 -0500

    Add project files.

---
+++ RetroVideoGameStore/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Models;
+
+namespace RetroVideoGameStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // use category modle to make a fake list of 10 categories
+            // create an empty list of categories
+            var categories = new List<Category>();
+            // loop 10 times to create 10 categories
+            for (int i = 1; i<= 10; i++)
+            {
+                categories.Add(new Category
+                {
+                    CategoryId = i,
+                    CategoryName = "Category " + i.ToString()
+                });
+            }
+        //pass the list to the view for display
+            return View(categories);
+        }
+        public IActionResult Browse(string categoryName)
+        {
+            // Grab the category name passed in with the URL
+            ViewBag.CategoryName = categoryName;
+            return View();
+
+        }
+    }
+}
